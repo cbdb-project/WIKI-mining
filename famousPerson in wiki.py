c52f7a0812226cef0a0f8a4a3c24da43ef977c6a
Hongsu Wang <sudospace@gmail.com>
2024-06-20 19:35:04 -0400

    fix http code decision issue

--- famousPerson in wiki.py
+++ famousPerson in wiki.py
@@ -23,7 +23,7 @@ def get_wiki_status(keywords):
         url = url_prefix + keyword
         response = requests.head(url)
         time.sleep(0.1)
-        if response.status_code == 200:
+        if response.status_code != 404:
             output.append([keyword, "1"])
         else:
             output.append([keyword, "0"])
